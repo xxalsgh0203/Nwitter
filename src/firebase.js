12c5ffd11be7cc1eac44e8011ca8089452fc5d18
Minho Kim <music4537@naver.com>
2021-12-31 00:24:51 -0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkH2Jz16mP5jzM01PU43738QF9r7NhTa0",
+    authDomain: "nwitter-e852d.firebaseapp.com",
+    projectId: "nwitter-e852d",
+    storageBucket: "nwitter-e852d.appspot.com",
+    messagingSenderId: "617366583947",
+    appId: "1:617366583947:web:79096b94fcdb6418c2f28e"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
